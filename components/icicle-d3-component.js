6164206cda18cee1e2f0ec904359b660b9bdfe04
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-05-21 07:29:34 +0200

    Animated resizing of icicle plots

--- components/icicle-d3-component.js
+++ components/icicle-d3-component.js
@@ -4,18 +4,13 @@ import * as d3 from 'd3';
 
 const size = 600
 
-let element
+let element, root
 
 class IcicleD3Component extends D3Component {
   initialize(node, props) {
-    let root = d3.hierarchy(props.data);
+    root = d3.hierarchy(props.data);
 
-    root.eachAfter(
-      d => d.value = d.children ? d.children.reduce(
-        (sum, child) => sum + child.value
-        , 0
-      ) : d.data.size
-    )
+    root.count()
 
     d3.partition()
       .size([600, 600])
@@ -43,10 +38,34 @@ class IcicleD3Component extends D3Component {
   }
 
   update(props, oldProps) {
+    if (props.index > 1) {
+      root.eachAfter(
+        d => d.value = d.children ? d.children.reduce(
+          (sum, child) => sum + child.value
+          , 0
+        ) : d.data.size
+      )
+    } else {
+      root.count()
+    }
+
+    d3.partition()
+      .size([600, 600])
+      (root);
+
+    element
+      .transition()
+      .duration(props.config.animationDuration)
+      .attr("transform", d => `translate(${d.x0},${d.y0})`)
+
     element.selectAll("rect")
       .transition()
       .duration(props.config.animationDuration)
+      .attr("width", d => d.x1 - d.x0)
       .style("fill", d => props.index > 0 ? props.config.fileTypeColors[d.data.name.split(".")[1]] : "black")
+
+    element.selectAll("text")
+      .text(d => d.data.name.substring(0, (d.x1 - d.x0) / 10))
   }
 }
 
